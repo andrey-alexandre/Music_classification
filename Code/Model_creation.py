3d59e0a7c78a0a189f6613fbba82ea19e08d07e8
Andrey Alexandre <andreyalexandre16@gmail.com>
2022-01-18 21:18:24 -0300

    Finalizada a etapa inicial do projeto

--- Code/Model_creation.py
+++ Code/Model_creation.py
@@ -2,7 +2,11 @@ import re
 import string
 import pandas as pd
 import spacy
-
+from sklearn.base import TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from joblib import dump
 
 df = pd.read_csv('./Data/full.csv', encoding='utf-8-sig')
 
@@ -15,8 +19,9 @@ rep_punctuations = '!{2,}|"{2,}|#{2,}|\${2,}|%{2,}|&{2,}|\'{2,}|({2,}|){2,}|\*{2
 whitespaces = string.whitespace
 
 # Create our list of stopwords
-nlp = spacy.load('pt_core_news_lg')
+nlp = spacy.load('pt_core_news_sm')
 stop_words = spacy.lang.pt.stop_words.STOP_WORDS
+
 
 # Creating our tokenizer function
 def spacy_tokenizer(sentence):
@@ -39,21 +44,45 @@ def spacy_tokenizer(sentence):
     # return preprocessed list of tokens
     return mytokens
 
+
+# Custom transformer using spaCy
+class predictors(TransformerMixin):
+    def transform(self, X, **transform_params):
+        # Cleaning Text
+        return [clean_text(text) for text in X]
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+
+
 # Basic function to clean the text
 def clean_text(text):
     # Removing spaces and converting text into lowercase
     return text.strip().lower()
 
 
-nlp = spacy.load('pt_core_news_lg')
-# print(len(b))
-# print(len(list(b.sents)))
-# print([sent for sent in b.sents])
+class feature_extraction(TransformerMixin):
+    def transform(self, X, **transform_params):
+        return [clean_text(text) for text in X]
 
-df['lyric_doc'] = df['lyric'].apply(lambda x: nlp(x))
-df['word_count'] = df['lyric_doc'].apply(lambda x: len(x))
-df['sent_count'] = df['lyric_doc'].apply(lambda x: len(list(x.sents)))
+    def fit(self, X, y=None, **fit_params):
+        return self
 
-df[['word_count', 'Class']].boxplot()
+    def get_params(self, deep=True):
+        return {}
 
-df.to_csv('./Data/plot.csv')
+
+bow_vector = CountVectorizer(tokenizer=spacy_tokenizer, ngram_range=(1, 1))
+tfidf_vector = TfidfVectorizer(tokenizer=spacy_tokenizer)
+
+X = df['lyric']  # the features we want to analyze
+y = df['Class']  # the labels, or answers, we want to test against
+
+pipe = Pipeline([("cleaner", predictors()),
+                 ('vectorizer', tfidf_vector),
+                 ('classifier', LogisticRegression())])
+pipe.fit(X, y)
+dump(pipe, './Docs/Models/lr_clf.joblib')
--- Code/model_selection.py
+++ Code/model_selection.py
@@ -1,14 +1,20 @@
 import re
 import string
 from datetime import datetime
-import numpy as np
 import pandas as pd
+import numpy as np
 import spacy
+from sklearn.utils import shuffle
 from sklearn import metrics
 from sklearn.base import TransformerMixin
+from sklearn.model_selection import KFold
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.pipeline import Pipeline
 
 df = pd.read_csv('./Data/full.csv', encoding='utf-8-sig')
@@ -66,11 +72,9 @@ def clean_text(text):
     # Removing spaces and converting text into lowercase
     return text.strip().lower()
 
-# Custom transformer using spaCy
+
 class feature_extraction(TransformerMixin):
     def transform(self, X, **transform_params):
-        df = pd.DataFrame.sparse.from_spmatrix(X)
-
         return [clean_text(text) for text in X]
 
     def fit(self, X, y=None, **fit_params):
@@ -79,90 +83,28 @@ class feature_extraction(TransformerMixin):
     def get_params(self, deep=True):
         return {}
 
+
 bow_vector = CountVectorizer(tokenizer=spacy_tokenizer, ngram_range=(1, 1))
 tfidf_vector = TfidfVectorizer(tokenizer=spacy_tokenizer)
 
 X = df['lyric']  # the features we want to analyze
 y = df['Class']  # the labels, or answers, we want to test against
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-# tfidf_vector.fit_transform(X_train)
-# print(sorted(tfidf_vector.get_feature_names()))
-#
-# lr = LogisticRegression()
-#
-# # Create pipeline using Bag of Words
-# lr_pipe = Pipeline([("cleaner", predictors()),
-#                  ('vectorizer', bow_vector),
-#                  ('classifier', lr)])
-#
-# # model generation
-# lr_pipe.fit(X_train, y_train)
-#
-# # Predicting with a test dataset
-# lr_predicted = lr_pipe.predict(X_test)
-#
-# # Model Accuracy
-# print("Logistic Regression Accuracy:", metrics.accuracy_score(y_test, lr_predicted))
-# # print("Logistic Regression Precision:", metrics.precision_score(y_test, lr_predicted))
-# print("Logistic Regression Recall:", metrics.recall_score(y_test, lr_predicted, average='micro'))
-#
-#
-# svm = LinearSVC()
-#
-# # Create pipeline using Bag of Words
-# svm_pipe = Pipeline([("cleaner", predictors()),
-#                  ('vectorizer', bow_vector),
-#                  ('classifier', svm)])
-#
-# # model generation
-# svm_pipe.fit(X_train, y_train)
-#
-# # Predicting with a test dataset
-# svm_predicted = svm_pipe.predict(X_test)
-#
-# # Model Accuracy
-# print("Logistic Regression Accuracy:", metrics.accuracy_score(y_test, svm_predicted))
-# # print("Logistic Regression Precision:", metrics.precision_score(y_test, lr_predicted))
-# print("Logistic Regression Recall:", metrics.recall_score(y_test, svm_predicted, average='micro'))
-#
-# nb = MultinomialNB()
-#
-# # Create pipeline using Bag of Words
-# nb_pipe = Pipeline([("cleaner", predictors()),
-#                  ('vectorizer', bow_vector),
-#                  ('classifier', nb)])
-#
-# # model generation
-# nb_pipe.fit(X_train, y_train)
-#
-# # Predicting with a test dataset
-# nb_predicted = nb_pipe.predict(X_test)
-#
-# # Model Accuracy
-# print("Logistic Regression Accuracy:", metrics.accuracy_score(y_test, nb_predicted))
-# # print("Logistic Regression Precision:", metrics.precision_score(y_test, lr_predicted))
-# print("Logistic Regression Recall:", metrics.recall_score(y_test, nb_predicted, average='micro'))
-
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+X, y = shuffle(X, y)
+X = X.reset_index(drop=True)
+y = y.reset_index(drop=True)
+kf = KFold(n_splits=10)
+kf.get_n_splits(X)
 
 names = [
     "Logistic Regression",
     "Nearest Neighbors",
     "Linear SVM",
     "RBF SVM",
-    # "Gaussian Process",
     "Decision Tree",
     "Random Forest",
-    "Neural Net",
-    "AdaBoost",
-    # "Naive Bayes",
-    # "QDA",
+    # "Neural Net",
+    # "AdaBoost"
 ]
 
 classifiers = [
@@ -170,40 +112,53 @@ classifiers = [
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025),
     SVC(gamma=2, C=1),
-    # GaussianProcessClassifier(1.0 * RBF(1.0)),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    MLPClassifier(alpha=1, max_iter=1000),
-    AdaBoostClassifier(),
-    # GaussianNB(),
-    # QuadraticDiscriminantAnalysis(),
+    # MLPClassifier(alpha=1, max_iter=1000),
+    # AdaBoostClassifier()
 ]
 
 clf_dict = dict()
 time_dict = dict()
-predicted_dict = dict()
-score_dict = dict()
-acc_dict = dict()
-recall_dict = dict()
-cm_dict = dict()
+kfold_predicted_dict = {name: list() for name in names}
+kfold_score_dict = {name: list() for name in names}
+kfold_acc_dict = {name: list() for name in names}
+kfold_recall_dict = {name: list() for name in names}
+kfold_cm_dict = {name: list() for name in names}
 for name, clf in zip(names, classifiers):
+    i = 0
     time_dict[name] = list()
     time_dict[name].append(datetime.now())
     pipe = Pipeline([("cleaner", predictors()),
                      ('vectorizer', tfidf_vector),
                      ('classifier', clf)])
 
-    pipe.fit(X_train, y_train)
-    clf_dict[name] = pipe
-    # Predicting with a test dataset
-    score_dict[name] = pipe.score(X_test, y_test)
-    predicted_dict[name] = pipe.predict(X_test)
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        pipe.fit(X_train, y_train)
+        clf_dict[name] = pipe
 
-    # Model Accuracy
-    acc_dict[name] = metrics.accuracy_score(y_test, predicted_dict[name])
-    cm_dict[name] = metrics.confusion_matrix(y_test, predicted_dict[name])
-    recall_dict[name] = metrics.recall_score(y_test, predicted_dict[name], average=None)
+        # Predicting with a test dataset
+        kfold_score_dict[name].append(pipe.score(X_test, y_test))
+        kfold_predicted_dict[name].append(pipe.predict(X_test))
+
+        # Model Accuracy
+        kfold_acc_dict[name].append(metrics.accuracy_score(y_test, kfold_predicted_dict[name][i]))
+        kfold_cm_dict[name].append(metrics.confusion_matrix(y_test, kfold_predicted_dict[name][i]))
+        kfold_recall_dict[name].append(metrics.recall_score(y_test, kfold_predicted_dict[name][i], average='macro'))
+
+        i += 1
     time_dict[name].append(datetime.now())
+print("Terminou de rodar!")
+
+score_dict = {name: list() for name in names}
+acc_dict = {name: list() for name in names}
+recall_dict = {name: list() for name in names}
+for name in names:
+    score_dict[name] = np.mean(kfold_score_dict[name])
+    acc_dict[name] = np.mean(kfold_acc_dict[name])
+    recall_dict[name] = np.mean(kfold_recall_dict[name])
 
 for name in names:
     print(f"O tempo de execução para o modelo {name} é ", time_dict[name][1] - time_dict[name][0])
